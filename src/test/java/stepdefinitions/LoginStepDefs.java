67936b72d771c3479c7690728a87e7e9bdfa9d00
Ghazal Aslami <gaslami@nelta.de>
2024-11-17 13:29:03 +0100

    TEst Commit Nr2

--- src/test/java/stepdefinitions/LoginStepDefs.java
+++
@@ -1,8 +0,0 @@
-package stepdefinitions;
-
-public class LoginStepDefs {
-    public LoginStepDefs() {
-        Given("^I am on the website \"([^\"]*)\"$", (String arg0) -> {
-        });
-    }
-}
--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -5,10 +5,23 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
 public class LoginSteps {
-
-    public void visitWebsite {
-
+    @Given("I am on the website {string}")
+    public void visitWebsite(String url) {
+        // Diese Methode wird ausgeführt, wenn der Schritt "Given I am on the website 'URL'" in der Feature-Datei aufgerufen wird.
+        // {string} bedeutet, dass ein String-Parameter erwartet wird. Hier ist es die URL der Website.
+        System.out.println("Website wird aufgerufen: " + url);
     }
 
+    @When("I click on tab {string}} and fill with valid credentials")
+    public  void ClickOnTabAndFillWithCredentials(String tab) {
+        // Diese Methode wird ausgeführt, wenn der Schritt "When I click on tab 'Log in' and fill with valid credentials" in der Feature-Datei aufgerufen wird.
+        // Der Parameter {string} bezieht sich hier auf den Namen des Tabs, z.B. "Log in".
+        System.out.println("Tab wird angeklickt und Felder werden befüllt " + tab);
+    }
+
+    @Then("I will be successfully logged in and redirected to the homepage")
+    public void RedirectingToHomepage() {
+        System.out.println("Login ist erfolgreich");
+    }
 
 }
